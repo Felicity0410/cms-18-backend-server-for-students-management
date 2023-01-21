38957e4be078c03e3b99a5fac7cc982590085ab7
Felicity Liu <felicityliu0410@gmail.com>
2023-01-21 12:15:28 +1030

    create all files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const v1Router = require('./routes')
+const PORT = process.env.PORT || 3000
+const app = express()
+app.use(cors())
+
+app.use('/v1', v1Router)
+app.listen(PORT, ()=>{
+    console.log(`server listening on port ${PORT}`);
+})
---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+const {Router} = require('express')
+const v1Router = Router()
+
+module.exports = v1Router
